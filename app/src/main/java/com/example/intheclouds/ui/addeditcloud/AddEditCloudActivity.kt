d590f51f3ac43a83d4d74c97f0455ab980c84e58
Gisela Chodos <gisela.chodos@stgconsulting.com>
2020-05-22 10:39:09 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/intheclouds/ui/addeditcloud/AddEditCloudActivity.kt
@@ -0,0 +1,13 @@
+package com.example.intheclouds.ui.addeditcloud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.intheclouds.R
+
+class AddEditCloudActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_cloud)
+    }
+}
