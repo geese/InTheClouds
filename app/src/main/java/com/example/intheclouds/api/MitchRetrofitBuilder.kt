5520a131aaf14cc34ab36c13936e0a637e4b6f1e
Geese <gisela.chodos@gmail.com>
2020-05-23 16:02:58 -0600

    Coding along with Mitch:  up through "LiveData Call Adapter for Retrofit", about to start "Repository Setup"
    Added "MitchApiService" and "MitchRetrofitBuilder" so I can rip them out if they are not working.  Hopefully they will work, though!

--- app/src/main/java/com/example/intheclouds/api/MitchRetrofitBuilder.kt
+++ app/src/main/java/com/example/intheclouds/api/MitchRetrofitBuilder.kt
@@ -1,5 +1,6 @@
 package com.example.intheclouds.api
 
+import com.codingwithmitch.mviexample.util.LiveDataCallAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -12,11 +13,12 @@ object MitchRetrofitBuilder {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(LiveDataCallAdapterFactory())
     }
 
-    val apiService: ApiService by lazy{
+    val apiService: MitchApiService by lazy{
         RetrofitBuilder.retrofitBuilder
             .build()
-            .create(ApiService::class.java)
+            .create(MitchApiService::class.java)
     }
 }
