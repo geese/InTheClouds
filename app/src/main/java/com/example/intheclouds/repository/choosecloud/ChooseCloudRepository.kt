9228b7988c1eb0314f6f2c3bae8752941d8cefd0
Geese <gisela.chodos@gmail.com>
2020-05-23 19:09:06 -0600

    Coding along with Mitch:  up through "NetworkBoundResource", about to start "DataState Listener"

--- app/src/main/java/com/example/intheclouds/repository/choosecloud/ChooseCloudRepository.kt
+++ app/src/main/java/com/example/intheclouds/repository/choosecloud/ChooseCloudRepository.kt
@@ -1,51 +1,28 @@
 package com.example.intheclouds.repository.choosecloud
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
 import com.example.intheclouds.api.MitchRetrofitBuilder
 import com.example.intheclouds.model.Cumulus
+import com.example.intheclouds.repository.NetworkBoundResource
 import com.example.intheclouds.ui.choosecloud.state.ChooseCloudViewState
-import com.example.intheclouds.util.ApiEmptyResponse
-import com.example.intheclouds.util.ApiErrorResponse
-import com.example.intheclouds.util.ApiSuccessResponse
-import com.example.intheclouds.util.DataState
+import com.example.intheclouds.util.*
 
 object ChooseCloudRepository {
 
     fun getCloudImages(): LiveData<DataState<ChooseCloudViewState>> {
 
-        // TEMPORARY !! just to get the ball rolling
-
-        return Transformations
-            .switchMap(MitchRetrofitBuilder.apiService.getCumulusPhotos()) { apiResponse ->
-                object: LiveData<DataState<ChooseCloudViewState>>(){
-                    override fun onActive() {
-                        super.onActive()
-                        when(apiResponse) {
-
-                            is ApiSuccessResponse -> {
-                                value = DataState.data(
-                                    message = null,
-                                    data = ChooseCloudViewState(
-                                        cloudImages = apiResponse.body.cloudImages as ArrayList<Cumulus.CloudImage>
-                                    )
-                                )
-                            }
-
-                            is ApiErrorResponse -> {
-                                value = DataState.error(
-                                    message = apiResponse.errorMessage
-                                )
-                            }
-
-                            is ApiEmptyResponse -> {
-                                value = DataState.error(
-                                    message = "HTTP 204. Returned NOTHING!!"
-                                )
-                            }
-                        }
-                    }
-                }
+        return object : NetworkBoundResource<Cumulus.Response, ChooseCloudViewState>() {
+            override fun handleApiSuccessResponse(response: ApiSuccessResponse<Cumulus.Response>) {
+                result.value = DataState.data(
+                    data = ChooseCloudViewState(
+                        cloudImages = response.body.cloudImages as ArrayList<Cumulus.CloudImage>
+                    )
+                )
             }
+
+            override fun createCall(): LiveData<GenericApiResponse<Cumulus.Response>> {
+                return MitchRetrofitBuilder.apiService.getCumulusPhotos()
+            }
+        }.asLiveData()
     }
 }
